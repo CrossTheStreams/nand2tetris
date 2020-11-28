f68a734b289c432708a30e8ddfb54e16d4ef95b6
Andrew Hautau <arhautau@gmail.com>
2020-11-28 14:34:50 -0800

    multiplication program for chapter 4

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,23 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0    // Set R2 to 0
+
+(LOOP)
+@R1
+D=M    // D=R1
+@END   
+D;JLE  // If R1 <= 0, goto END
+@R0
+D=M    // D=R0
+@R2
+M=D+M  // R2=R2+R0
+@R1
+M=M-1  // R1=R1-1
+@LOOP
+0;JMP  // Goto LOOP
+
+(END)
+@END
+0;JMP  // Infinite loop
